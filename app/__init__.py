e460dcd00fe9481a1e1ea2f0e7180d32195c8f62
Justin Lafferty <lafferjm@gmail.com>
2019-04-19 10:04:04 -0500

    work in progress api

--- app/__init__.py
+++ app/__init__.py
@@ -29,6 +29,9 @@ app.register_blueprint(auth_bp, url_prefix='/auth')
 from app.main import bp as main_bp # noqa
 app.register_blueprint(main_bp)
 
+from app.api import bp as api_bp # noqa
+app.register_blueprint(api_bp, url_prefix='/api')
+
 if not app.debug:
     if app.config['MAIL_SERVER']:
         auth = None
--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 from time import time
 from datetime import datetime
 from hashlib import md5
+from flask import url_for
 import jwt
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
@@ -74,6 +75,33 @@ class User(UserMixin, db.Model):
         except: # noqa
             return
         return User.query.get(id)
+
+    def to_dict(self, include_email=False):
+        data = {
+            'id': self.id,
+            'username': self.username,
+            'last_seen': self.last_seen.isoformat() + 'Z',
+            'about_me': self.about_me,
+            'post_count': self.posts.count(),
+            'follower_count': self.followers.count(),
+            'followed_count': self.followed.count(),
+            '_links': {
+                'self': url_for('api.get_user', id=self.id),
+                'followers': url_for('api.get_followers', id=self.id),
+                'followed': url_for('api.get_followed', id=self.id),
+                'avatar': self.avatar(128)
+            }
+        }
+        if include_email:
+            data['email'] = self.email
+        return data
+
+    def from_dict(self, data, new_user=False):
+        for field in ['username', 'email', 'about_me']:
+            if field in data:
+                setattr(self, field, data[field])
+        if new_user and 'password' in data:
+            self.set_password(data['password'])
 
 
 class Post(db.Model):
